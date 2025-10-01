93caceb112d8d57ebb48bd6bcb1af0bb0262f5d5
palllaura <palllaura.tln@gmail.com>
2025-10-01 11:09:04 +0300

    Initial commit

---
+++ src/main/java/com/laurapall/sectorselect/SectorSelectApplication.java
@@ -0,0 +1,13 @@
+package com.laurapall.sectorselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorSelectApplication.class, args);
+	}
+
+}
